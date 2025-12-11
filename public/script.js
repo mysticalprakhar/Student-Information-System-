ebe8097f053c020155634e718c847ca804dfc8c8
mysticalprakhar <pandeyprakhar920@gmail.com>
2025-12-11 23:23:00 +0530

    Implement add and search student functions

--- public/script.js
+++ public/script.js
@@ -1,1 +1,41 @@
+const API = window.location.origin;
 
+// ADD STUDENT
+async function addStudent() {
+    const data = {
+        roll: document.getElementById("roll").value,
+        name: document.getElementById("name").value,
+        age: document.getElementById("age").value,
+        courses: document.getElementById("courses").value.split(",").map(c => c.trim())
+    };
+
+    await fetch(`${API}/add`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    });
+
+    alert("Student Added!");
+}
+
+// SEARCH
+async function searchStudent() {
+    const key = document.getElementById("searchKey").value;
+
+    const res = await fetch(`${API}/search/${key}`);
+    const data = await res.json();
+
+    const box = document.getElementById("results");
+    box.innerHTML = "";
+
+    data.forEach(s => {
+        box.innerHTML += `
+            <div class='student-box'>
+                <b>${s.name}</b><br>
+                Roll: ${s.roll}<br>
+                Age: ${s.age}<br>
+                Courses: ${s.courses.join(", ")}
+            </div>
+        `;
+    });
+}
